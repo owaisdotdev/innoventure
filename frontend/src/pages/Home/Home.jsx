72308fe0da7cd66ace17133138ac7bfa079d313e
asad <shaikhasad183@gmail.com>
2024-10-03 09:26:59 +0500

    feature

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import Hero from "./Hero/Hero"
 import About from "./About/About"
 import React from 'react'
 import FeaturedStartups from "./Featured/Featured"
+import Features from "./Features/Features"
 
 const Home = () => {
   return (
     <div><Hero/>
     <About/>
     <FeaturedStartups/>
+    <Features/>
     </div>
   )
 }
