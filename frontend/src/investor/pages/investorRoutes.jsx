8f96671fb38fdf6b560e664239b22103fead2098
Awais Zahid <zahidawais31@gmail.com>
2025-02-17 16:13:22 +0500

    Task 2

--- frontend/src/investor/pages/investorRoutes.jsx
+++ frontend/src/investor/pages/investorRoutes.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 
 import { Routes, Route } from 'react-router-dom';
-
-import { Route } from 'react-router-dom';
-
-
 import InvestmentPortfolio from './InvestmentPortfolio';
 import FeaturedStartups from './Featured';
 import RecommendedStartups from './Recommended';
